4e1d91b80ee36e2ae305a5bcf5f772b8759b0802
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2015-05-10 10:33:41 -0400

    More unit tests for UserService
    1. findUserByVerificationToken - retrieves a user by its verification
    token
    2. findUserByPasswordResetToken - retrieves a user by its password reset
    token
    3. save - persists a user entity

--- src/test/java/com/kossyuzokwe/service/UserServiceTest.java
+++ src/test/java/com/kossyuzokwe/service/UserServiceTest.java
@@ -14,10 +14,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import com.kossyuzokwe.dao.PasswordResetTokenRepository;
 import com.kossyuzokwe.dao.UserRepository;
+import com.kossyuzokwe.dao.VerificationTokenRepository;
+import com.kossyuzokwe.model.PasswordResetToken;
 import com.kossyuzokwe.model.User;
+import com.kossyuzokwe.model.VerificationToken;
 import com.kossyuzokwe.test.MockValues;
+import com.kossyuzokwe.test.PasswordResetTokenFactoryForTest;
 import com.kossyuzokwe.test.UserFactoryForTest;
+import com.kossyuzokwe.test.VerificationTokenFactoryForTest;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
@@ -28,7 +34,17 @@ public class UserServiceTest {
 	@Mock
 	private UserRepository userRepository;
 	
+	@Mock
+	private VerificationTokenRepository verificationTokenRepository;
+	
+	@Mock
+	private PasswordResetTokenRepository resetTokenRepository;
+	
 	private UserFactoryForTest userFactoryForTest = new UserFactoryForTest();
+	
+	private VerificationTokenFactoryForTest verificationTokenFactoryForTest = new VerificationTokenFactoryForTest();
+	
+	private PasswordResetTokenFactoryForTest resetTokenFactoryForTest = new PasswordResetTokenFactoryForTest();
 
 	private MockValues mockValues = new MockValues();
 
@@ -89,6 +105,51 @@ public class UserServiceTest {
 		// Then
 		assertEquals(user.getUserEmail(),userFound.getUserEmail());
 	}
+
+	@Test
+	public void findUserByVerificationToken() {
+		// Given
+		User user = userFactoryForTest.newUser();
+		VerificationToken verificationToken = verificationTokenFactoryForTest.newVerificationToken(user);
+		String token = verificationToken.getToken();
+		when(verificationTokenRepository.findByToken(token)).thenReturn(verificationToken);
+		
+		// When
+		User userFound = userService.findUserByVerificationToken(token);
+		
+		// Then
+		assertEquals(user.getUserId(), userFound.getUserId());
+	}
+
+	@Test
+	public void findUserByPasswordResetToken() {
+		// Given
+		User user = userFactoryForTest.newUser();
+		PasswordResetToken passwordResetToken = resetTokenFactoryForTest.newPasswordResetToken(user);
+		String token = passwordResetToken.getToken();
+		when(resetTokenRepository.findByToken(token)).thenReturn(passwordResetToken);
+		
+		// When
+		User userFound = userService.findUserByPasswordResetToken(token);
+		
+		// Then
+		assertEquals(user.getUserId(), userFound.getUserId());
+	}
+
+	@Test
+	public void save() {
+		// Given
+		User user = userFactoryForTest.newUser();
+		when(userRepository.save(user)).thenReturn(user);
+		
+		// When
+		User userResult = userService.save(user);
+
+		// Then
+		assertEquals(userResult.getUserId(), user.getUserId());
+	}
+
+	@Test
 	public void deleteUser() {
 		// Given
 		String id = mockValues.nextId();
