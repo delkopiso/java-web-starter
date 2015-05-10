0645f2f5c4dae253ca33b2728ffd0b19ac50bd67
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2015-05-09 22:35:43 -0400

    Added unit tests for UserService
    Wrote tests for the following methods/behaviors in UserService:
    1. findAll - retrieves every user entity in the database
    2. findUserByUserId - retrieve a user entity given its id
    3. deleteUser - removes the user whose id is specified from the database

---
+++ src/test/java/com/kossyuzokwe/service/UserServiceTest.java
@@ -0,0 +1,79 @@
+package com.kossyuzokwe.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.kossyuzokwe.dao.UserRepository;
+import com.kossyuzokwe.model.User;
+import com.kossyuzokwe.test.MockValues;
+import com.kossyuzokwe.test.UserFactoryForTest;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+
+	@InjectMocks
+	private UserService userService;
+	
+	@Mock
+	private UserRepository userRepository;
+	
+	private UserFactoryForTest userFactoryForTest = new UserFactoryForTest();
+
+	private MockValues mockValues = new MockValues();
+
+	@Test
+	public void findAll() {
+		// Given		
+		User user1 = userFactoryForTest.newUser();
+		User user2 = userFactoryForTest.newUser();
+		List<User> users = Arrays.asList(user1, user2);
+		when(userRepository.findAll()).thenReturn(users);
+
+		// When
+		List<User> usersFounds = new ArrayList<User>(userService.findAll());
+
+		// Then
+		assertTrue(user1 == usersFounds.get(0));
+		assertTrue(user2 == usersFounds.get(1));
+	}
+
+	@Test
+	public void findUserByUserId() {
+		// Given
+		User user = userFactoryForTest.newUser();
+		String id = user.getUserId();
+		when(userRepository.findByUserId(id)).thenReturn(user);
+
+		// When
+		User userFound = userService.findUserByUserId(id);
+
+		// Then
+		assertEquals(user.getUserId(),userFound.getUserId());
+	}
+
+	@Test
+	public void deleteUser() {
+		// Given
+		String id = mockValues.nextId();
+
+		// When
+		userService.deleteUser(id);
+
+		// Then
+		verify(userRepository).delete(id);
+	}
+
+}
+
